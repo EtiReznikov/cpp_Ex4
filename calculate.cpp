15b8536cefe1ded33892a3c8b51fd55080afccb9
etireznikov <etirez56@gmail.com>
2019-04-30 16:51:15 +0300

    calc

--- calculate.cpp
+++ calculate.cpp
@@ -4,12 +4,12 @@
 string calculateBullAndPgia(string choice, string guess)
 {
     int bull=0, pgia=0;
-    bool bullindex[choice.length()];
-    bool pgiaindex[choice.length()];
+    bool guessindex[choice.length()];
+    bool choiceindex[choice.length()];
     for (int k=0; k<choice.length(); k++)
     {
-bullindex[k]=false;
-pgiaindex[k]=false;
+guessindex[k]=false;
+choiceindex[k]=false;
     }
         
 
@@ -18,19 +18,21 @@ pgiaindex[k]=false;
        if (choice[k]==guess[k])
        {
            bull++;
-           bullindex[k]=true;
+           guessindex[k]=true;
+           choiceindex[k]=true;
        }
    }
    int i=0, j=0;
    while (i<choice.length())
    {
-       if (!bullindex[i] && !pgiaindex[i]){
+       if (!guessindex[i] && !choiceindex[i]){
        if (choice[i]==guess[j])
        {
            if (i!=j )
            {
             pgia++;
-            pgiaindex[i]=true;
+             guessindex[i]=true;
+           choiceindex[i]=true;
             i++;
             j=0;
            }
